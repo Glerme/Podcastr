c4cee123eaed58e0b71d4cdd256960c2ffe6566e
Guilherme Godoi <guiggff@gmail.com>
2021-04-21 11:25:36 -0300

    :sparkles: Segundo Dia Completo

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import { NextPage } from "next";
+import format from "date-fns/format";
+import ptBr from "date-fns/locale/pt-BR";
+
+import styles from "./styles.module.scss";
+
+const Header: NextPage = () => {
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", {
+    locale: ptBr,
+  });
+
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr" />
+      <p>O melhor para você ouvir, sempre!</p>
+
+      <span>{currentDate}</span>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-function Home() {
-  return <div>hello World</div>;
+function Home(props) {
+  console.log(props.episodes);
+
+  return (
+    <>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
+  );
 }
+
+export const getStaticProps = async () => {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+};
 
 export default Home;
