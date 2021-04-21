6118013acf928ca7f9f230dcca1fb2b0229138b0
Guilherme Godoi <guiggff@gmail.com>
2021-04-21 15:20:48 -0300

    Començando a terceira aula

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,35 @@
-function Home(props) {
-  console.log(props.episodes);
+import { GetStaticProps, NextPage } from "next";
+
+type HomeProps = {
+  episodes: Array<Episode>;
+};
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  published_at: string;
+  thumbnail: string;
+  description: string;
+  file: {
+    url: string;
+    type: string;
+    duration: number;
+  };
+};
+
+const Home: NextPage<HomeProps> = ({ episodes }) => {
+  console.log(episodes);
 
   return (
     <>
       <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
+      <p>{JSON.stringify(episodes)}</p>
     </>
   );
-}
+};
 
-export const getStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await fetch("http://localhost:3333/episodes");
   const data = await response.json();
 
